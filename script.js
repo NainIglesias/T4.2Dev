904406c08fd651124c36881c0437150e0f65f75e
NainIglesias <naioptamo00@gmail.com>
2024-02-06 10:53:12 +0100

    Fix: cors problem

--- script.js
+++ script.js
@@ -5,10 +5,12 @@ let image = document.getElementById('image');
 const monsterName = document.getElementById('searched');
 const apiURL = 'https://mhw-db.com/monsters/';
 
-promise.addEventListener('click',()=>{
-    alert('soy una promesa');
+
+function fetchPromise(){
     const getURL = apiURL + monsterName.innerText;
-    fetch(getURL)
+    alert(getURL);
+    console.log(getURL);
+    fetch(apiURL)
         .then(response =>{
             if(!response.ok){
                 throw new Error('La solicitud no pudo ser completada');
@@ -17,10 +19,24 @@ promise.addEventListener('click',()=>{
         })
         .then(responseData =>{
             text.innerText = 'respuesta promise';
-
-
         })
+}
+
+async function asyncAwait(){
+
+    const getURL = apiURL + monsterName.innerText;
+    let reponse = await fetch(getURL);
+    let data = await reponse.json();
+    console.log(reponse);
+    console.log('data',data);
+}
+
+
+promise.addEventListener('click',()=>{
+    //alert('soy una promesa');
+    fetchPromise();
 })
 async.addEventListener('click',()=>{
-    alert('soy una async');
+   // alert('soy una async');
+    asyncAwait();
 })
