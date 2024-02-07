cbe9604fe6f67f5b72986b2cf60f4df2ebc85303
NainIglesias <naioptamo00@gmail.com>
2024-02-07 14:28:22 +0100

    finishing errors

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 const promise = document.getElementById('promise');
 const async = document.getElementById('async');
-let text = document.getElementById('texto');
 let image = document.getElementById('image');
-const monsterName = document.getElementById('searched').value;
-const url = 'https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/'+monsterName;
+const url = 'https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/';
 const options = {
     method: 'GET',
     headers: {
@@ -15,23 +13,24 @@ const options = {
 
 function fetchPromise(){
     // alert(getURL);
-    // console.log(getURL);
-    fetch(url, options)
+    // console.log(getURL)
+
+    const monsterName = document.getElementById('searched').value;
+    fetch(url+monsterName, options)
         .then(response =>{
             if(!response.ok){
-                throw new Error('La solicitud no pudo ser completada');
+                throw new Error('Carta no encontrada');
             }
             return response.json();
         })
         .then(responseData =>{
             //text.innerText = 'respuesta promise';
             //console.log(responseData)
-            responseData.forEach(element => {
-                if(element.hasOwnProperty('imgGold')){
-                    image.setAttribute('src', element.imgGold);
-                    text.innerText = element.faction;
-                }
-            });
+                responseData.forEach(element => {
+                    if(element.hasOwnProperty('img')){
+                        image.setAttribute('src', element.img);
+                     }
+             });
             // image.setAttribute('src',responseData[1].imgGold);
 
         })
@@ -39,17 +38,25 @@ function fetchPromise(){
 
 async function asyncAwait(){
 
-    const reponse = await fetch(url,options);
+    const monsterName = document.getElementById('searched').value;
+    const reponse = await fetch(url+monsterName,options);
     const responseData = await reponse.json();
     // console.log(reponse);
-    //console.log('data',data);
+    //console.log('data',data)
+    //console.log(responseData.length);
+    if(responseData.length>=0){
 
-    responseData.forEach(element => {
-        if(element.hasOwnProperty('imgGold')){
-            image.setAttribute('src', element.imgGold);
-            text.innerText = element.faction;
-        }
-    });
+
+        responseData.forEach(element => {
+            if(element.hasOwnProperty('img')){
+                image.setAttribute('src', element.img);
+                //validarImagen(element);
+                // break;
+            }
+        });
+    }else{
+            alert('carta no encontrada');
+    }
 }
 
 
@@ -61,3 +68,6 @@ async.addEventListener('click',()=>{
     // alert('soy una async');
     asyncAwait();
 })
+
+
+
