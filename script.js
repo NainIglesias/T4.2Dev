c4aa817dc245f3777cf9bcb3661ae0c3003b778a
NainIglesias <naioptamo00@gmail.com>
2024-02-07 13:03:27 +0100

    change the conection to another api

--- script.js
+++ script.js
@@ -2,15 +2,21 @@ const promise = document.getElementById('promise');
 const async = document.getElementById('async');
 let text = document.getElementById('texto');
 let image = document.getElementById('image');
-const monsterName = document.getElementById('searched');
-const apiURL = 'https://mhw-db.com/monsters/';
+const monsterName = document.getElementById('searched').value;
+const url = 'https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/'+monsterName;
+const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '137265fd30mshce5b2112f9ef1c9p1faffejsnc2350ba2913d',
+        'X-RapidAPI-Host': 'omgvamp-hearthstone-v1.p.rapidapi.com'
+    }
+};
 
 
 function fetchPromise(){
-    const getURL = apiURL + monsterName.innerText;
-    alert(getURL);
-    console.log(getURL);
-    fetch(apiURL)
+    // alert(getURL);
+    // console.log(getURL);
+    fetch(url, options)
         .then(response =>{
             if(!response.ok){
                 throw new Error('La solicitud no pudo ser completada');
@@ -18,17 +24,23 @@ function fetchPromise(){
             return response.json();
         })
         .then(responseData =>{
-            text.innerText = 'respuesta promise';
+            //text.innerText = 'respuesta promise';
+            //console.log(responseData);
+            text.innerText = responseData[0].faction;
+            image.setAttribute('src',responseData[0].imgGold);
+
         })
 }
 
 async function asyncAwait(){
 
-    const getURL = apiURL + monsterName.innerText;
-    let reponse = await fetch(getURL);
-    let data = await reponse.json();
-    console.log(reponse);
-    console.log('data',data);
+    const reponse = await fetch(url,options);
+    const responseData = await reponse.json();
+    // console.log(reponse);
+    //console.log('data',data);
+
+    text.innerText = responseData[0].faction;
+    image.setAttribute('src',responseData[0].imgGold);
 }
 
 
@@ -37,6 +49,6 @@ promise.addEventListener('click',()=>{
     fetchPromise();
 })
 async.addEventListener('click',()=>{
-   // alert('soy una async');
+    // alert('soy una async');
     asyncAwait();
 })
